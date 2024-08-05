6f2e9633e1c2b9f97999aa91b7f0d7f09373bf81
wonc2 <114809413+wonc2@users.noreply.github.com>
2024-08-05 16:10:48 +0900

    Update Notice_DML_insert.sql

--- sqls/Notice_DML_insert.sql
+++ sqls/Notice_DML_insert.sql
@@ -13,21 +13,21 @@ INSERT INTO `NOTICE` (`NOTICE_PK`, `WRITER_PK`, `CONTENT`) VALUES
 ('NOT05', 'WR03', '서비스 이용약관 변경 안내');
 
 -- VISITORS 테이블에 데이터 삽입
-INSERT INTO `VISITORS` (`VISITOR_PK`, `NOTICE_PK`, `WRITER_PK`, `VISITOR_NAME`) VALUES
+INSERT INTO `VISITORS` (`VISITOR_PK`, `NOTICE_PK`, `VISITOR_NAME`) VALUES
 -- NOTICE_PK: NOT01
-('VI_01', 'NOT01', 'WR01', '홍길동'),
-('VI_02', 'NOT01', 'WR01', '김영희'),
+('VI_01', 'NOT01', '홍길동'),
+('VI_02', 'NOT01', '김영희'),
 -- NOTICE_PK: NOT02
-('VI_03', 'NOT02', 'WR02', '백지영'),
-('VI_04', 'NOT02', 'WR02', '최민호'),
-('VI_05', 'NOT02', 'WR02', '송지현'),
+('VI_03', 'NOT02', '백지영'),
+('VI_04', 'NOT02', '최민호'),
+('VI_05', 'NOT02', '송지현'),
 -- NOTICE_PK: NOT03
-('VI_06', 'NOT03', 'WR01', '장성우'),
+('VI_06', 'NOT03', '장성우'),
 -- NOTICE_PK: NOT04
-('VI_07', 'NOT04', 'WR03', '백지영'),
-('VI_08', 'NOT04', 'WR03', '최민호'),
-('VI_09', 'NOT04', 'WR03', '임현서'),
+('VI_07', 'NOT04', '백지영'),
+('VI_08', 'NOT04', '최민호'),
+('VI_09', 'NOT04', '임현서'),
 -- NOTICE_PK: NOT05
-('VI_10', 'NOT05', 'WR03', '강수민'),
-('VI_11', 'NOT05', 'WR03', '백지영'),
-('VI_12', 'NOT05', 'WR03', '박정희');
+('VI_10', 'NOT05', '강수민'),
+('VI_11', 'NOT05', '백지영'),
+('VI_12', 'NOT05', '박정희');
