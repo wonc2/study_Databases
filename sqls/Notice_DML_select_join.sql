d37ec7e3ea0625b09495993a40f3c716a519c97e
wonc2 <114809413+wonc2@users.noreply.github.com>
2024-08-07 10:52:19 +0900

    Update Notice_DML_select_join.sql

--- sqls/Notice_DML_select_join.sql
+++ sqls/Notice_DML_select_join.sql
@@ -1,6 +1,6 @@
 -- db_notices
 
-SELECT CONTENT as '공지내용', WRITER as '작성자', COUNT(*) as `방문자 명수`
+SELECT CONTENT as '공지내용', WRITER as '작성자', COUNT(VISITOR) as `방문자 명수`
 from Notice n
 INNER JOIN Writers w ON w.WRITER_PK = n.WRITER_FK
 INNER JOIN Visitors v ON n.NOTICE_PK = v.NOTICE_FK
